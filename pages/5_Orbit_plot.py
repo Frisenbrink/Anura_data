d2ae44d2e07fb038aa23a3ca0694744b59c1157d
Frisenbrink <93766324+Frisenbrink@users.noreply.github.com>
2023-02-20 22:13:10 +0100

    Orbit plot start

--- pages/5_Orbit_plot.py
+++ pages/5_Orbit_plot.py
@@ -1,31 +1,34 @@
 import streamlit as st
 import numpy as np #Imports Python mathematical functions library
-import matplotlib
-#matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-
-
+import mpld3
+import streamlit.components.v1 as components
 
 logo = "Materials/ReVibe.png"
 
 video = "Materials/Vibinspect_demo.mp4"
 zip = "Anura_data.zip"
 
+#x = np.genfromtxt("Circular_motion/00_80_e1_26_e7_16/1673620685.csv", delimiter=",", skip_header=2, usecols=[0])
+#y = np.genfromtxt("Circular_motion/00_80_e1_26_e7_16/1673620685.csv", delimiter=",", skip_header=2, usecols=[1])
 
 st.columns(3)[1].image(logo, width=250)
 
 st.header("Orbit plot")
 
-cos = np.cos
-pi = np.pi
+R = 1
 
-a = 5
-e = 0.3
-theta = np.linspace(0,2*pi, 360)
-r = (a*(1-e**2))/(1+e*cos(theta))
+n = 63
+
+t = np.linspace(0, 2*np.pi, n+1)
+
+x = R*np.cos(t)
+y = R*np.sin(t)
+
 fig = plt.figure()
-plt.polar(theta, r)
-
-print(np.c_[r,theta])
-
-plt.show()
+plt.axis("equal")
+plt.grid()
+plt.plot(x, y)
+fig_html = mpld3.fig_to_html(fig)
+components.html(fig_html, height=800)
+    
